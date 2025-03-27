0d02a58b87227e6b6ca3bbd86b25937d3b9186b5
kkaminska45 <k.kaminska04@wp.pl>
2025-03-27 13:54:41 +0100

    ładowanie obrazu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnLoad = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(24, 258);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(131, 39);
+            btnLoad.TabIndex = 0;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveBorder;
+            pictureBox1.Location = new Point(192, 34);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(469, 263);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(696, 326);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnLoad);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnLoad;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,26 @@ namespace LAB4
         {
             InitializeComponent();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Title = "Wybierz obraz",
+                Filter = "Pliki BMP (*.bmp)|*.bmp"
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(openFile.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("B³¹d wczytywania obrazu: " + ex.Message, "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
     }
 }
